c4324844040feb643c8fbfaffe02c7f72dec66c1
Diego Rodriguez <diegorrod91@gmail.com>
2019-11-12 20:28:18 -0300

    V 1.0.0

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: ['./src/index'],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom',
+    },
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules|packages/,
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ]
+      }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin(), new webpack.NamedModulesPlugin()],
+};
